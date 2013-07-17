4b793f3d55d02dcc8400878f52c1e84791c67206
Axel Bodart <axel.bodart@gmail.com>
2013-07-17 20:45:54 +0200

    Switch to runtime only version of Handlebars

--- assets/make.sh
+++ assets/make.sh
@@ -3,5 +3,6 @@
 #lessc css/style.less css/style.min.css
 handlebars template/resume.handlebars -f js/resume.min.js -m
 lessc --yui-compress css/style.less css/style.min.css
+uglifyjs js/handlebars.runtime.js -o js/handlebars.runtime.min.js
 uglifyjs js/me-fr.js -o js/me-fr.min.js
 uglifyjs js/me-en.js -o js/me-en.min.js
