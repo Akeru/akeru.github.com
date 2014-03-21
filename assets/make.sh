6254c21f30708b7d10d350ffe57974603c34c197
Akeru <axel.bodart@gmail.com>
2014-03-21 13:31:25 +0100

    Fix icon style Fix typo Compress pictures

--- assets/make.sh
+++ assets/make.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-#handlebars template/resume.handlebars -f js/resume.min.js
 handlebars template/resume.handlebars -f js/resume.min.js -m
-#lessc css/style.less css/style.min.css
-lessc --yui-compress css/style.less css/style.min.css
-uglifyjs js/handlebars.runtime.js -o js/handlebars.runtime.min.js
+lessc --clean-css css/style.less css/style.min.css
 uglifyjs js/me-fr.js -o js/me-fr.min.js
 uglifyjs js/me-en.js -o js/me-en.min.js
